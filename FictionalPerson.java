17ab097286974987fc6d80a7d08f3a5d309e9fad
sami-whitten <samantha.whitten@csuglobal.edu>
2024-08-18 16:55:33 -0400

    Add files via upload

---
+++ FictionalPerson.java
@@ -0,0 +1,14 @@
+package x;
+
+public class FictionalPerson {
+	public static void main(String args[])
+	{
+		String newline = System.lineSeparator();
+		System.out.print("First name: Mickey" + newline + 
+				"Last name: Mouse"+ newline + 
+				"Street address: P.O. Box 10040" + newline +
+				"City: Lake Buena Vista, FL" + newline +
+				"Zip Code: 32830-0040");		
+	}
+
+}
